391629f17e2675464a1ffa4db8968f8f6b61e171
Carson Wright <carsonnwright@gmail.com>
2014-12-18 11:28:03 -0500

    added tests

--- tests/main_spec.rb
+++ tests/main_spec.rb
@@ -9,9 +9,30 @@ scope do
   end
   test "Homepage" do
     get "/home"
-    assert_equal "<!DOCTYPE html>\n<html lang='en'>\n  <head>\n    <meta charset='utf-8'>\n    <meta content='IE=edge' http-equiv='X-UA-Compatible'>\n    <title>Here is the title</title>\n  </head>\n  <body class='page-index'>\n    \n    HERE\n    \n  </body>\n</html>\n", last_response.body
+    page_html = "<!DOCTYPE html>\n<html lang='en'>\n  <head>\n    <meta charset='utf-8'>\n    <meta content='IE=edge' http-equiv='X-UA-Compatible'>\n    <title>\"The Title\"</title>\n  </head>\n  <body class='page-index'>\n    \n    \n    \n  </body>\n</html>\n"
+    assert_equal page_html, last_response.body
   end
   test "Embargo" do
   	assert_equal Embargo.application, Cuba
+  end
+  test "Object Blank?" do
+    o = Object.new
+    assert_equal o.blank?, false
+  end
+  test "Humanize String" do
+    string = "Here_Is".humanize
+    assert_equal string, "Here Is"
+  end
+  test "Humanize Symbol" do
+    string = :Here_Is.humanize
+    assert_equal string, "Here Is"
+  end
+  test "Embargo Directory" do
+    directory = Embargo.app_directory
+    assert_equal directory, Dir.pwd
+  end
+  test "Embargo Directory" do
+    directory = Embargo.directory
+    assert_equal directory.match("embargo.rb").to_s, "embargo.rb"
   end
 end
